04618e907117e513e71e8240e1827163369634cb
Coolgeek789 <coolman_789@hotmail.com>
2021-05-25 09:55:44 +0200

    fixed small window height

--- widgets/open-tickets/data.js
+++ widgets/open-tickets/data.js
@@ -6,7 +6,7 @@ function loadPage()
   jQuery.ajax("./src/index.php?widgetId=" + widgetId + "&page=" + pageNumber, {
     success: function (htmlData) {
       jQuery("#openTicketsTable").empty().append(htmlData);
-      var hostMonitoringTable = jQuery("#openTicketsTable").find("img, style, script, link").load(function () {
+      var hostMonitoringTable = jQuery("#openTicketsTable").find("img, style, script, link").on('load', function () {
         var h = document.getElementById("openTicketsTable").scrollHeight + 50;
         parent.iResize(window.name, h);
       });
